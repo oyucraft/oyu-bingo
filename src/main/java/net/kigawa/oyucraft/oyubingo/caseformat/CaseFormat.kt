b02713d1c0e646dd3e50ba2bc4801c5d3053634b
kigawa <kigawa.8390@gmail.com>
2023-06-11 19:33:54 +0900

    add: case format

--- src/main/java/net/kigawa/oyucraft/oyubingo/caseformat/CaseFormat.kt
+++ src/main/java/net/kigawa/oyucraft/oyubingo/caseformat/CaseFormat.kt
@@ -1,13 +1,52 @@
 package net.kigawa.oyucraft.oyubingo.caseformat
 
-enum class CaseFormat {
-  KEBAB_CASE,
-  LOWER_CAMEL_CASE,
-  HIGHER_CAMEL_CASE;
-  fun marge(values: List<String>): String{
+@Suppress("unused")
+enum class CaseFormat(
+  private val marge: (List<String>)->String,
+  private val split: (String)->List<String>,
+) {
   
+  KEBAB_CASE({
+    it.joinToString("-")
+  }, {
+    it.split("-")
+  }),
+  HIGHER_CAMEL_CASE({values->
+    val sb = StringBuilder()
+    values.forEach {
+      if (it == "") return@forEach
+      
+      sb.append(it[0].uppercaseChar()).append(it.drop(1))
+    }
+    sb.toString()
+  }, {string->
+    val list = mutableListOf<String>()
+    val sb = StringBuilder()
+    
+    string.forEach {
+      if (it.isLowerCase()) {
+        sb.append(it)
+        return@forEach
+      }
+      if (sb.length != 0) {
+        list.add(sb.toString())
+        sb.clear()
+      }
+      sb.append(it)
+    }
+    
+    listOf()
+  });
+  
+  fun newInstance(values: List<String>): CaseString {
+    return CaseString(values, this)
   }
-  fun split(string: String): List<String>{
   
+  fun marge(values: List<String>): String {
+    return marge.invoke(values)
+  }
+  
+  fun split(string: String): List<String> {
+    return split.invoke(string)
   }
 }
--- src/main/java/net/kigawa/oyucraft/oyubingo/caseformat/CaseString.kt
+++ src/main/java/net/kigawa/oyucraft/oyubingo/caseformat/CaseString.kt
@@ -1,11 +1,11 @@
 package net.kigawa.oyucraft.oyubingo.caseformat
 
-interface CaseString {
-  val values: List<String>
-  fun split(): List<String>{
-    return values.toMutableList()
-  }
-  fun marge(): String{
-    return marge(values)
+class CaseString(
+  val values: List<String>,
+  private val caseFormat: CaseFormat,
+) {
+  
+  override fun toString(): String {
+    return caseFormat.marge(values)
   }
 }
